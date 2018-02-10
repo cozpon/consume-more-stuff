fd7912e29f3b51a0be3a7eb01796f47f18c804df
Ricky Peters <ericrickyallenpeters@gmail.com>
2018-02-10 11:46:17 -1000

    pushing changes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+
+const compression = require('compression');
+const expressValidator = require('express-validator');
+const flash = require('express-flash');
+
 const passport = require('passport');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -8,14 +13,17 @@ const authenticatePassport = require('./lib/passport');
 const db = require('./models');
 const routes = require('./routes');
 
+
 const PORT = process.env.PORT || 8888;
 
 const app = express();
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 // enabling json body-parser and encoding
 app.use(bodyParser.urlencoded({ "extended" : false }));
 app.use(bodyParser.json());
+app.use(expressValidator());
 app.use(session({
   store: new redis(),
   secret: "keyboard cat",
@@ -25,7 +33,7 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(flash());
 app.use('/api', routes);
 
 app.listen(PORT, () => {
